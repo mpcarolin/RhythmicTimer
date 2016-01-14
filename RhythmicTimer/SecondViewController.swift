01dd605a05debc0fece65cd388a4bf7e8ba41820
Michael <mpcarolin@gmail.com>
2016-01-14 14:41:19 -0700

    Initial Commit

---
+++ RhythmicTimer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RhythmicTimer
+//
+//  Created by Michael Carolin on 1/14/16.
+//  Copyright © 2016 Michael Carolin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
